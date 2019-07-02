d945cb0d0efc5ce0704adbe4b1616b46ca36fba1
Pratikbhai Patel <pratikpatel@me.com>
2019-07-02 16:13:04 -0700

    Add Spanish and French prefixes

--- hello.go
+++ hello.go
@@ -2,16 +2,34 @@ package main
 
 import "fmt"
 
+const english = "English"
 const englishHelloPrefix = "Hello, "
 
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hola, "
+
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
+
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "English"))
 }
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	return englishHelloPrefix + name
+	prefix := ""
+
+	switch language {
+	case english:
+		prefix = englishHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	}
+
+	return prefix + name
 }
