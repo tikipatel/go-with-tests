dcf6a0f6914cd2100a7fea929cd059ba052953ed
Pratikbhai Patel <pratikpatel@me.com>
2019-07-02 12:27:31 -0700

    If name is empty, then return 'Hello, World'

--- hello.go
+++ hello.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func main() {
 	fmt.Println(Hello("world"))
 }
 
 func Hello(name string) string {
-	return "Hello, " + name + "!"
+	if name == "" {
+		name = "World"
+	}
+
+	return englishHelloPrefix + name
 }
