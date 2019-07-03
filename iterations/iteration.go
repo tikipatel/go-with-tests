99838466636db4d5bc402a39bcde8b226aee9502
Pratikbhai Patel <pratikpatel@me.com>
2019-07-03 10:37:04 -0700

    Iterations

---
+++ iterations/iteration.go
@@ -0,0 +1,9 @@
+package iterations
+
+func Repeat(character string, repeatCount int) string {
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated = repeated + character
+	}
+	return repeated
+}
---
+++ iterations/iteration_test.go
@@ -0,0 +1,12 @@
+package iterations
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected '%s' but got '%s'", expected, repeated)
+	}
+}
