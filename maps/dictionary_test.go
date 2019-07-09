f0a79c9d32a7eae8c9c87526000393c0dfbce4a7
Pratikbhai Patel <pratikpatel@me.com>
2019-07-09 14:29:33 -0700

    Initial dicitionary

---
+++ maps/dictionary_test.go
@@ -0,0 +1,18 @@
+package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	got := Search(dictionary, "test")
+	want := "this is just a test"
+	assetStrings(t, got, want)
+}
+
+func assetStrings(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
