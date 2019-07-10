43a99fac41f4d9391d1b7c9528a66a39e5bdc989
Pratikbhai Patel <pratikpatel@me.com>
2019-07-10 14:30:18 -0700

    extended walk test

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -1,6 +1,9 @@
 package reflection
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
 	expected := "Chris"
@@ -20,5 +23,34 @@ func TestWalk(t *testing.T) {
 
 	if len(got) != 1 {
 		t.Errorf("wrong nubmer of function calls, got %d, wanted %d", len(got), 1)
+	}
+}
+
+func TestWalkExtended(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
