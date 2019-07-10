70f22c9482f29282b90cb1173626394db9a6f24b
Pratikbhai Patel <pratikpatel@me.com>
2019-07-10 14:45:26 -0700

    passing pointers

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -17,6 +17,10 @@ type Profile struct {
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
 
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 		switch field.Kind() {
--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -34,7 +34,7 @@ func TestWalkExtended(t *testing.T) {
 	}{
 		{
 			"Nested fields",
-			Person{
+			&Person{
 				"Chris",
 				Profile{33, "London"},
 			},
