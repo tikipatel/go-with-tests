06f989d35e9727e2bb1cb1d5e4542a68fa8ba331
Pratikbhai Patel <pratikpatel@me.com>
2019-07-11 14:06:11 -0700

    Roman 5 --> V

--- property_texts/property_texts.go
+++ property_texts/property_texts.go
@@ -4,14 +4,19 @@ import "strings"
 
 // ConvertToRoman is a function
 func ConvertToRoman(arabic int) string {
-	if arabic == 4 {
-		return "IV"
-	}
-
 	var result strings.Builder
 
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if i == 5 {
+			result.WriteString("V")
+			break
+		}
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
+
 	return result.String()
 }
