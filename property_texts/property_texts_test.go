63745615d876789f878865a588103d3297567e01
Pratikbhai Patel <pratikpatel@me.com>
2019-07-11 14:25:08 -0700

    refactor and make it all work

--- property_texts/property_texts_test.go
+++ property_texts/property_texts_test.go
@@ -1,6 +1,7 @@
 package propertytexts
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -31,12 +32,67 @@ func TestRomanNumerals(t *testing.T) {
 		{"18 gets converted to XVIII", 18, "XVIII"},
 		{"20 gets converted to XX", 20, "XX"},
 		{"39 gets converted to XXXIX", 39, "XXXIX"},
+		{"40 gets converted to XL", 40, "XL"},
+		{"47 gets converted to XLVII", 47, "XLVII"},
+		{"49 gets converted to XLIX", 49, "XLIX"},
+		{"50 gets converted to L", 50, "L"},
 	}
 
 	for _, test := range cases {
 		t.Run(test.Description, func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
 			assertCorrectNumeral(t, got, test.Want)
+		})
+	}
+
+	t.Run("special case 1984 is converted to MCMLXXXIV", func(t *testing.T) {
+		got := ConvertToRoman(1984)
+		want := "MCMLXXXIV"
+		assertCorrectNumeral(t, got, want)
+	})
+}
+
+func TestRomanNumeralsQuii(t *testing.T) {
+	cases := []struct {
+		Arabic int
+		Roman  string
+	}{
+		{Arabic: 1, Roman: "I"},
+		{Arabic: 2, Roman: "II"},
+		{Arabic: 3, Roman: "III"},
+		{Arabic: 4, Roman: "IV"},
+		{Arabic: 5, Roman: "V"},
+		{Arabic: 6, Roman: "VI"},
+		{Arabic: 7, Roman: "VII"},
+		{Arabic: 8, Roman: "VIII"},
+		{Arabic: 9, Roman: "IX"},
+		{Arabic: 10, Roman: "X"},
+		{Arabic: 14, Roman: "XIV"},
+		{Arabic: 18, Roman: "XVIII"},
+		{Arabic: 20, Roman: "XX"},
+		{Arabic: 39, Roman: "XXXIX"},
+		{Arabic: 40, Roman: "XL"},
+		{Arabic: 47, Roman: "XLVII"},
+		{Arabic: 49, Roman: "XLIX"},
+		{Arabic: 50, Roman: "L"},
+		{Arabic: 100, Roman: "C"},
+		{Arabic: 90, Roman: "XC"},
+		{Arabic: 400, Roman: "CD"},
+		{Arabic: 500, Roman: "D"},
+		{Arabic: 900, Roman: "CM"},
+		{Arabic: 1000, Roman: "M"},
+		{Arabic: 1984, Roman: "MCMLXXXIV"},
+		{Arabic: 3999, Roman: "MMMCMXCIX"},
+		{Arabic: 2014, Roman: "MMXIV"},
+		{Arabic: 1006, Roman: "MVI"},
+		{Arabic: 798, Roman: "DCCXCVIII"},
+	}
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Roman {
+				t.Errorf("got %q, want %q", got, test.Roman)
+			}
 		})
 	}
 }
